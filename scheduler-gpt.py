865a07b26e35fbb9353711f4f4217f511a32101e
vincent <vincent>
2024-02-18 23:59:14 -0500

    plz

--- scheduler-gpt.py
+++ scheduler-gpt.py
@@ -25,7 +25,7 @@ class Process:
         return max(start - self.arrival, 0)
 
 
-def fcfs(processes, runtime):
+def fcfs(processes, runtime, out):
     arrive_counter = 0
     finished_counter = 0
     running = Process("", 0, -1)
@@ -43,13 +43,13 @@ def fcfs(processes, runtime):
             proc = order[arrive_counter]
             waiting.append(proc)
             proc.wait = 0
-            print(f"Time {i:4} : {proc.name} arrived")
+            out.write(f"Time {i:4} : {proc.name} arrived\n")
             arrive_counter += 1
 
         if running.burst != -1:
             running.burst -= 1
             if running.burst == 0:
-                print(f"Time {i:4} : {running.name} finished")
+                out.write(f"Time {i:4} : {running.name} finished\n")
                 running.finish = i + 1
                 finished.append(running)
                 finished_counter += 1
@@ -63,31 +63,31 @@ def fcfs(processes, runtime):
                     running.start = i
                     running.has_started = True
                     running.wait = i - running.arrival
-                print(
-                    f"Time {i:4} : {running.name} selected (burst {running.burst})")
+                out.write(
+                    f"Time {i:4} : {running.name} selected (burst {running.burst})\n")
             else:
-                print(f"Time {i:4} : Idle")
+                out.write(f"Time {i:4} : Idle\n")
 
-    print(f"Finished at time {runtime}\n")
+    out.write(f"Finished at time {runtime}\n")
 
     for finished_proc in finished:
         for proc in processes:
             if finished_proc.name == proc.name:
                 proc.turnaround = finished_proc.finish - proc.arrival
 
-    calculate_metrics_fcfs(processes)
+    calculate_metrics_fcfs(processes, out)
     return finished
 
 
-def calculate_metrics_fcfs(processes):
+def calculate_metrics_fcfs(processes, out):
     for process in processes:
         turnaround = process.turnaround_time(process.finish - 1)
         response = process.response_time(process.start)
-        print(
-            f"{process.name} wait {process.wait:4} turnaround {turnaround:4} response {response:4}")
+        out.write(
+            f"{process.name} wait {process.wait:4} turnaround {turnaround:4} response {response:4}\n")
 
 
-def calculate_metrics_rr(processes, response_time_diff):
+def calculate_metrics_rr(processes, response_time_diff, out):
     total_turnaround_time = 0
     total_response_time = 0
     total_waiting_time = 0
@@ -108,27 +108,27 @@ def calculate_metrics_rr(processes, response_time_diff):
         process.turnaround = turnaround
         process.response = response
 
-        print(
-            f"{process.name} wait {wait:4} turnaround {turnaround:4} response {response:4}")
+        out.write(
+            f"{process.name} wait {wait:4} turnaround {turnaround:4} response {response:4}\n")
 
    # average_turnaround_time = total_turnaround_time / len(processes)
     #average_response_time = total_response_time / len(processes)
     #average_waiting_time = total_waiting_time / len(processes)
 
-   # print(f"\nAverage Turnaround Time: {average_turnaround_time:.2f}")
-   # print(f"Average Response Time: {average_response_time:.2f}")
-   # print(f"Average Waiting Time: {average_waiting_time:.2f}")
+   # out.write(f"\nAverage Turnaround Time: {average_turnaround_time:.2f}")
+   # out.write(f"Average Response Time: {average_response_time:.2f}")
+   # out.write(f"Average Waiting Time: {average_waiting_time:.2f}")
 
 
-def rr(processes, runtime, quantum):
+def rr(processes, runtime, quantum, out):
     waiting = []
     finished = []
     current_time = 0
     selected_times = {process.name: [] for process in processes}
 
-    # print(f"{len(processes)} processes")
-    print("Using Round-Robin")
-    print(f"Quantum {quantum}\n")
+    # out.write(f"{len(processes)} processes")
+    out.write("Using Round-Robin\n")
+    out.write(f"Quantum {quantum}\n")
 
     # Sort processes based on arrival time
     processes.sort(key=lambda p: p.arrival)
@@ -142,7 +142,7 @@ def rr(processes, runtime, quantum):
                 if not process.has_started:
                     process.start = current_time
                     process.has_started = True
-                    print(f"Time {current_time:4} : {process.name} arrived")
+                    out.write(f"Time {current_time:4} : {process.name} arrived\n")
 
         if waiting:
             current_process = waiting.pop(0)
@@ -150,8 +150,8 @@ def rr(processes, runtime, quantum):
                 current_process.response = current_time - current_process.arrival
                 # Append the arrival, selected time, and response time to the list
                 selected_times[current_process.name].append((current_process.arrival, current_time, current_process.response))
-            print(
-                f"Time {current_time:4} : {current_process.name} selected (burst {current_process.remaining})")
+            out.write(
+                f"Time {current_time:4} : {current_process.name} selected (burst {current_process.remaining})\n")
 
             if current_process.remaining > quantum:
                 current_process.remaining -= quantum
@@ -163,29 +163,29 @@ def rr(processes, runtime, quantum):
                 current_process.finish = current_time
                 current_process.remaining = 0
                 finished.append(current_process)
-                print(
-                    f"Time {current_time:4} : {current_process.name} finished")
+                out.write(
+                    f"Time {current_time:4} : {current_process.name} finished\n")
         else:
             if processes or waiting:  # Check if there are processes left or waiting processes
                 current_time += 1
 
-    # Print "Idle" with respective times at the end
+    # out.write "Idle" with respective times at the end
     while current_time < runtime:
-        print(f"Time {current_time:4} : Idle")
+        out.write(f"Time {current_time:4} : Idle\n")
         current_time += 1
 
-    print(f"Finished at time {runtime}\n")
+    out.write(f"Finished at time {runtime}\n")
 
     response_time_dif = []
     for process_name, times_list in selected_times.items():
         response_time_dif.extend([response for _, _, response in times_list])
 
 
-    calculate_metrics_rr(finished, response_time_dif)
+    calculate_metrics_rr(finished, response_time_dif, out)
     return finished
 
 
-def sjf(processes, runtime):
+def sjf(processes, runtime, out):
     arrive_counter = 0
     wait_counter = 0
     finished_counter = 0
@@ -205,7 +205,7 @@ def sjf(processes, runtime):
             proc = order[arrive_counter]
             waiting.append(proc)
             proc.wait = 0
-            print(f"Time {i:4} : {proc.name} arrived")
+            out.write(f"Time {i:4} : {proc.name} arrived\n")
 
             burst_time[processes.index(proc)] = proc.burst  # Written by human
             arrive_counter += 1
@@ -217,7 +217,7 @@ def sjf(processes, runtime):
         if running.burst != -1:
             running.burst -= 1
             if running.burst == 0:
-                print(f"Time {i:4} : {running.name} finished")
+                out.write(f"Time {i:4} : {running.name} finished\n")
                 running.finish = i + 1
                 finished.append(running)
                 finished_counter += 1
@@ -231,95 +231,97 @@ def sjf(processes, runtime):
                     running.has_started = True
 
                     running.wait = i - running.arrival  # Update waiting time here
-                print(
-                    f"Time {i:4} : {running.name} selected (burst {running.burst})")
+                out.write(
+                    f"Time {i:4} : {running.name} selected (burst {running.burst})\n")
                 wait_counter -= 1
             else:
-                print(f"Time {i:4} : Idle")
+                out.write(f"Time {i:4} : Idle\n")
 
         elif wait_counter != 0 and running.burst > waiting[0].burst:
             temp = running
             running = waiting.pop(0)
             waiting.append(temp)
 
-            print(
-                f"Time {i:4} : {running.name} selected (burst {running.burst})")
+            out.write(
+                f"Time {i:4} : {running.name} selected (burst {running.burst})\n")
 
             waiting.sort(key=lambda p: p.burst)
 
-    print(f"Finished at time {runtime}\n")
+    out.write(f"Finished at time {runtime}\n")
 
     for finished_proc in finished:
         for proc in processes:
             if finished_proc.name == proc.name:
                 proc.turnaround = finished_proc.finish - proc.arrival
 
-    calculate_metrics_sjf(processes, burst_time)
+    calculate_metrics_sjf(processes, burst_time, out)
     return finished
 
 
-def calculate_metrics_sjf(processes, burst_time):
+def calculate_metrics_sjf(processes, burst_time, out):
     for process in processes:
         turnaround = process.turnaround_time(process.finish - 1)
         response = process.response_time(process.start)
         # Written by human
         wait = turnaround - burst_time[processes.index(process)]
-        print(
-            f"{process.name} wait {wait:4} turnaround {turnaround:4} response {response:4}")
+        out.write(
+            f"{process.name} wait {wait:4} turnaround {turnaround:4} response {response:4}\n")
 
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: scheduler-gpt.py <input file>")
+        out.write("Usage: scheduler-gpt.py <input file>\n")
         sys.exit(1)
     input_file = sys.argv[1]
+    output_file = input_file.replace('.in', '.out')
 
-    try:
-        with open(input_file, 'r') as file:
-            lines = file.readlines()
+    with open(output_file, 'w') as out:
+        try:
+            with open(input_file, 'r') as file:
+                lines = file.readlines()
 
-        process_count = int(lines[0].split()[1])
-        run_for = int(lines[1].split()[1])
-        algorithm = lines[2].split()[1]
+            process_count = int(lines[0].split()[1])
+            run_for = int(lines[1].split()[1])
+            algorithm = lines[2].split()[1]
 
-        if algorithm == 'rr':
-            try:
-                quantum = int(lines[3].split()[1])
-            except IndexError:
-                print("Error: missing quantum for rr algorithm")
-                sys.exit(1)
-            processes_data = lines[4:-1]
-        else:
-            quantum = None
-            processes_data = lines[3:-1]
+            if algorithm == 'rr':
+                try:
+                    quantum = int(lines[3].split()[1])
+                except IndexError:
+                    out.write("Error: missing quantum for rr algorithm\n")
+                    sys.exit(1)
+                processes_data = lines[4:-1]
+            else:
+                quantum = None
+                processes_data = lines[3:-1]
 
-        processes = []
-        for data in processes_data:
-            parts = data.split()
-            name = parts[2]
-            arrival = int(parts[4])
-            burst = int(parts[6])
-            processes.append(Process(name, arrival, burst))
+            processes = []
+            for data in processes_data:
+                parts = data.split()
+                name = parts[2]
+                arrival = int(parts[4])
+                burst = int(parts[6])
+                processes.append(Process(name, arrival, burst))
 
-        print(f"{process_count} processes")
-        if algorithm == 'sjf':
-            print("Using preemptive Shortest Job First")
-        elif algorithm == 'fcfs':
-            print("Using First-Come First-Served")
-        # else:  # elif algorithm == 'rr' and then add another else for edge cases
-        #     print(
-        #         f"Using {algorithm}{' Quantum ' + str(quantum) if algorithm == 'rr' and quantum else ''}")
+            out.write(f"{process_count} processes\n")
+            if algorithm == 'sjf':
+                out.write("Using preemptive Shortest Job First\n")
+            elif algorithm == 'fcfs':
+                out.write("Using First-Come First-Served\n")
+            # else:  # elif algorithm == 'rr' and then add another else for edge cases
+            #     out.write(
+            #         f"Using {algorithm}{' Quantum ' + str(quantum) if algorithm == 'rr' and quantum else ''}")
 
-        if algorithm == 'sjf':
-            sjf(processes, run_for)
-        elif algorithm == 'rr':
-            # rr(processes, run_for, quantum)
-            finished_processes = rr(processes, run_for, quantum)
-        elif algorithm == 'fcfs':
-            fcfs(processes, run_for)
-    except FileNotFoundError:
-        print(f"Error: file {input_file} not found")
-        sys.exit(1)
+            if algorithm == 'sjf':
+                sjf(processes, run_for, out)
+            elif algorithm == 'rr':
+                # rr(processes, run_for, quantum)
+                finished_processes = rr(processes, run_for, quantum, out)
+            elif algorithm == 'fcfs':
+                fcfs(processes, run_for, out)
+        except FileNotFoundError:
+            out.write(f"Error: file {input_file} not found\n")
+            sys.exit(1)
 
 
 if __name__ == "__main__":
